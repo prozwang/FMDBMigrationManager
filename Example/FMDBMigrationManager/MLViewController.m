0637555633cf6ce921833d3f867dd5deb8c9a997
sml <smlsjq@163.com>
2020-04-15 16:55:19 +0800

    Initial commit

---
+++ Example/FMDBMigrationManager/MLViewController.m
@@ -0,0 +1,29 @@
+//
+//  MLViewController.m
+//  FMDBMigrationManager
+//
+//  Created by qq912276337 on 04/15/2020.
+//  Copyright (c) 2020 qq912276337. All rights reserved.
+//
+
+#import "MLViewController.h"
+
+@interface MLViewController ()
+
+@end
+
+@implementation MLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
